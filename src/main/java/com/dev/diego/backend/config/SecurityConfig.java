cac3b93f1713adcdbb2e2ddc7629873a0bd43350
DiegoSS14 <sousadiego533@gmail.com>
2025-12-16 01:18:08 -0300

    Add Spring Security and configurate Cors

---
+++ src/main/java/com/dev/diego/backend/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.dev.diego.backend.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults())
+        .csrf(corsf-> corsf.disable())
+        .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth->auth.anyRequest().permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowCredentials(true); // Permite o envio de credenciais através dos headers, por exemplo tokens jwt
+        UrlBasedCorsConfigurationSource urlSource = new UrlBasedCorsConfigurationSource(); // variável que recebe toda a configuração feita anteriormente
+        urlSource.registerCorsConfiguration("/**", config);
+        return urlSource;
+    }
+}
